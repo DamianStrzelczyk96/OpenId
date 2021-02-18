c1935c1cf5e86623c76066c68b00ffd3a9a0d5c1
Your Name <damianstrzelczyk@o2.pl>
2021-02-18 17:01:39 +0100

    first commit

---
+++ src/main/java/openIde/TestApi.java
@@ -0,0 +1,37 @@
+package openIde;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.context.SecurityContextImpl;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import sun.plugin.liveconnect.SecurityContextHelper;
+
+import java.util.LinkedHashMap;
+
+@Controller
+public class TestApi {
+
+    @GetMapping("/test1")
+    public String test1(){
+        return "test1";
+    }
+    @GetMapping("/test2")
+    public String test2(Model model){
+
+       Object detalis =  ((UsernamePasswordAuthenticationToken)((OAuth2Authentication)((SecurityContextImpl)SecurityContextHolder.getContext()).getAuthentication()).getUserAuthentication()).getDetails();
+       String name = ((LinkedHashMap)detalis).values().toArray()[1].toString();
+        String picture = ((LinkedHashMap)detalis).values().toArray()[4].toString();
+        String email = ((LinkedHashMap)detalis).values().toArray()[5].toString();
+
+        model.addAttribute("name",name);
+        model.addAttribute("picture",picture);
+        model.addAttribute("email",email);
+
+        return  "gui";
+    }
+
+}
